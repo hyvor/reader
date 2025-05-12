32e13de94b22805c5af337b2828aec2821025a7a
Supun Wimalasena <supun@hyvor.com>
2025-05-12 14:29:33 +0200

    wip

--- backend/src/Api/App/Controller/MainController.php
+++ backend/src/Api/App/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Api\App\Controller;
 
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+
 class MainController
 {
     #[Route('/main', methods: ['GET'])]
