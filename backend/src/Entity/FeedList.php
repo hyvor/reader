31d6a4493910d741702446f509caafc515384727
Sakith B. <pvsakith@gmail.com>
2025-05-21 23:09:08 +0530

    chore: add feed list

---
+++ backend/src/Entity/FeedList.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FeedListRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FeedListRepository::class)]
+class FeedList
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+}
