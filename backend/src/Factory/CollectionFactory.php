2c42d8f20c3fa78ab4966d919b848256c056b446
Sakith B. <pvsakith@gmail.com>
2025-05-22 22:09:05 +0530

    chore: add factories

---
+++ backend/src/Factory/CollectionFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Collection;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<Collection>
+ */
+final class CollectionFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return Collection::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Collection $collection): void {})
+        ;
+    }
+}
