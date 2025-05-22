aec510e3810771e11b561f78efed94e866f14c79
Sakith B. <pvsakith@gmail.com>
2025-05-22 21:30:03 +0530

    refactor: refactored migration files and entity files

--- backend/migrations/Version20250519132321.php
+++ backend/migrations/Version20250519132321.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250519132321 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,18 +16,16 @@ final class Version20250519132321 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE feed_list (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))
+            CREATE TABLE feed_list (
+                id BIGSERIAL NOT NULL PRIMARY KEY,
+                name TEXT NOT NULL,
+            )
         SQL);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql(<<<'SQL'
-            CREATE SCHEMA public
-        SQL);
         $this->addSql(<<<'SQL'
             DROP TABLE feed_list
         SQL);
