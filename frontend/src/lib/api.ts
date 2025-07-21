81669a79e994e7ee55d58ac3620f7d6e540c44df
Sakith B. <pvsakith@gmail.com>
2025-07-21 16:56:07 +0530

    minor fixes

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,10 +2,7 @@ type Method = 'get' | 'post' | 'patch' | 'put' | 'delete';
 
 export default class api {
 
-    static async call(method: Method, endpoint: string, data: Record<string, any> = {}, customFetch?: typeof fetch) {
-
-        const fetchFn = customFetch || fetch;
-
+    static async call(method: Method, endpoint: string, data: Record<string, any> = {}) {
         const apiUrl = location.origin + "/api/app";
         const endpointUrl = endpoint.replace(/^\//, '');
         let url = `${apiUrl}/${endpointUrl}`;
@@ -17,7 +14,7 @@ export default class api {
             url += '?' + query;
         }
 
-        const response = await fetchFn(url, {
+        const response = await fetch(url, {
             method: method.toUpperCase(),
             body: method !== 'get' ? JSON.stringify(data) : undefined,
             headers: {
@@ -35,20 +32,20 @@ export default class api {
 
     }
 
-    static async get(url: string, data: Record<string, any> = {}, customFetch?: typeof fetch) {
-        return await api.call('get', url, data, customFetch);
+    static async get(url: string, data: Record<string, any> = {}) {
+        return await api.call('get', url, data);
     }
 
-    static async post(url: string, data: Record<string, any> = {}, customFetch?: typeof fetch) {
-        return await api.call('post', url, data, customFetch);
+    static async post(url: string, data: Record<string, any> = {}) {
+        return await api.call('post', url, data);
     }
 
-    static async patch(url: string, data: Record<string, any> = {}, customFetch?: typeof fetch) {
-        return await api.call('patch', url, data, customFetch);
+    static async patch(url: string, data: Record<string, any> = {}) {
+        return await api.call('patch', url, data);
     }
 
-    static async delete(url: string, data: Record<string, any> = {}, customFetch?: typeof fetch) {
-        return await api.call('delete', url, data, customFetch);
+    static async delete(url: string, data: Record<string, any> = {}) {
+        return await api.call('delete', url, data);
     }
 
 }
