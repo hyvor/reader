5c1471e761176a6a59565b6f7e88cd920a0ba01f
Sakith B. <pvsakith@gmail.com>
2025-10-06 16:24:28 +0530

    add monolog (#51)

---
+++ backend/config/packages/monolog.php
@@ -0,0 +1,31 @@
+<?php
+
+use Symfony\Config\MonologConfig;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (MonologConfig $monolog, ContainerConfigurator $container): void {
+    if ($container->env() !== 'test') {
+        $monolog->handler('app')
+            ->type('buffer')
+            ->handler('final')
+            ->level("%env(LOG_LEVEL)%")
+            ->bubble(false)
+            ->channels()->elements(['app']);
+        $monolog->handler('non_app')
+            ->type('buffer')
+            ->handler('final')
+            ->level('error')
+            ->bubble(false)
+            ->channels()->elements(['!app']);
+        $monolog->handler('final')
+            ->type('stream')
+            ->path('php://stderr')
+            ->formatter('monolog.formatter.json');
+    } else {
+        $monolog->handler('test')
+            ->type('test')
+            ->level('info');
+    }
+};
+
+
